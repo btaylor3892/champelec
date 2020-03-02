20883a05389283373ceba5274d64ac7d5a280d3c
Brian Taylor <btaylor3892@gmail.com>
2020-03-02 11:05:45 -0500

    Enlarge Services Page Image

--- src/templates/services-page.js
+++ src/templates/services-page.js
@@ -18,7 +18,7 @@ export const ServicePageTemplate = ({
 }) => (
   <div className="content">
     <div
-      className="full-width-image-container margin-top-0"
+      className="full-width-image margin-top-0"
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
