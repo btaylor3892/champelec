e73526042d90809882fbb430d3e560aeb05e327c
Brian Taylor <btaylor3892@gmail.com>
2020-03-06 15:59:13 -0500

    Move redirect file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,11 +16,6 @@ const TemplateWrapper = ({ children, location }) => {
         <meta name="description" content={description} />
         <meta charset="utf-8" />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
           rel="icon"
           type="image/png"
           href={`${withPrefix('/')}img/favicon-32x32.png`}
@@ -31,11 +26,6 @@ const TemplateWrapper = ({ children, location }) => {
           type="image/png"
           href={`${withPrefix('/')}img/favicon-16x16.png`}
           sizes="16x16"
-        />
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />
         <meta property="og:type" content="business.business" />
